199774f2fdf59e310d2ee72f17adc8332d0b7d32
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-10-16 09:40:04 -0400

    Initial Commit

---
+++ tic-tac-twirl/tic_tac_twirlApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_twirlApp.swift
+//  tic-tac-twirl
+//
+//  Created by Eric Chandonnet on 2024-10-16.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_twirlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
