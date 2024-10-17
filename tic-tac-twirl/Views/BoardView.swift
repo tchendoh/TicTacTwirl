22414d7ef2fa32552faab59d09fbc156acd63d4f
tchendoh <42325308+tchendoh@users.noreply.github.com>
2024-10-17 09:14:11 -0400

    Add empty files so the folders get added.

---
+++ tic-tac-twirl/Views/BoardView.swift
@@ -0,0 +1,18 @@
+//
+//  BoardView.swift
+//  tic-tac-twirl
+//
+//  Created by Eric Chandonnet on 2024-10-17.
+//
+
+import SwiftUI
+
+struct BoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BoardView()
+}
